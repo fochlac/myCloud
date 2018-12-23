1de68395dbabd4f21e1c940c8089360bc5cad5dd
fochlac <fochlac@gmail.com>
2018-12-23 22:38:33 +0100

    add memlogging

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,29 @@
 #!/usr/bin/env node
+
 import './config'
 
 import * as express from 'express'
 
 import { createServer } from 'http'
+import logger from './utils/logger'
 import router from './router/router'
+
+const log = (level, ...message) => logger(level, 'core -', ...message)
+const memwatch = require('memwatch-next')
 
 const app = express()
 const server = createServer(app)
 
 server.listen(global.port, global.address, () => {
-  console.log(`listening to http://${global.address}:${global.port}/`)
+  log(0, `listening to http://${global.address}:${global.port}/`)
 })
+
+memwatch.on('leak', (info) => {
+  log(1, 'Memory leak detected:\n', info);
+});
+
+memwatch.on('stats', function(stats) {
+  log(7, 'Garbage Collection ran:\n', stats)
+});
 
 app.use(router)
