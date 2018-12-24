56a248a8c194f37f7a9743b86885d1562517d67f
fochlac <fochlac@gmail.com>
2018-12-24 01:23:55 +0100

    remove memwatch

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@ import logger from './utils/logger'
 import router from './router/router'
 
 const log = (level, ...message) => logger(level, 'core -', ...message)
-const memwatch = require('memwatch-next')
 
 const app = express()
 const server = createServer(app)
@@ -17,13 +16,5 @@ const server = createServer(app)
 server.listen(global.port, global.address, () => {
   log(0, `listening to http://${global.address}:${global.port}/`)
 })
-
-memwatch.on('leak', (info) => {
-  log(1, 'Memory leak detected:\n', info);
-});
-
-memwatch.on('stats', function(stats) {
-  log(7, 'Garbage Collection ran:\n', stats)
-});
 
 app.use(router)
